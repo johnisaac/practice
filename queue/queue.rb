6efaf5b8ea85a0929f5dc7f1b796378a74259aca
John Felix <felix1985@gmail.com>
2011-08-30 20:12:29 +0530

    moved push, count to common.rb

--- queue/queue.rb
+++ queue/queue.rb
@@ -1,21 +1,16 @@
+require '../lib/common.rb'
+
 class Queue
+  include Common
+  
   attr_accessor :elements
   def initialize
     @elements = []
-  end
-  
-  def push( item )
-    @elements << item
-    @elements
   end
   
   def pop
     item = @elements[0]
     @elements.delete_at( 0 )
     item
-  end
-  
-  def count
-    @elements.count
   end
 end
--- stack/stack.rb
+++ stack/stack.rb
@@ -1,20 +1,16 @@
+require '../lib/common.rb'
+
 class Stack
+  include Common
+  
   attr_accessor :elements
   def initialize
     @elements = []
-  end
-  
-  def push( item )
-    @elements << item
   end
   
   def pop
     item = @elements[@elements.length - 1]
     @elements.delete_at( @elements.length - 1 )
     item
-  end
-  
-  def count
-    @elements.count
   end
 end
